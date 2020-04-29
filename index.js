48814970fd8bca62074d027302cd37778dec537a
bcharles <bcharles41078@gmail.com>
2020-04-29 12:13:11 -0500

    second

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 'use strict';
+const htmlStr = {
+  newStr: ''
+};
 
-function makeImageString(str) {
-  
-}
+
+
+
 
 function getDogImage(num) {
-    console.log(num);
-    console.log(typeof num);
+    
 
   for(let i = 0; i < num; i++) {
     fetch('https://dog.ceo/api/breeds/image/random')
       .then(response => response.json())
       .then(responseJson => {
-        console.log(responseJson.message);
+        makeImageString(responseJson.message);
+        console.log(htmlStr.newStr);
       });
   }
+  // console.log(htmlStr.newStr);
+  // render(htmlStr.newStr);
+}
+
+function makeImageString(str) {
+  
+  htmlStr.newStr = `${htmlStr.newStr}<br>
+  <img src='${str}'>`;
+  console.log(htmlStr.newStr);
+  $('.images').html(htmlStr.newStr);
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     let value = Number($('.js-text-input').val());
-    console.log(value);
-    console.log(typeof value);
-
+    
     if(isValidNumber(value)) {
       console.log('Entered valid number');
       getDogImage(value);
@@ -35,11 +46,17 @@ function watchForm() {
 }
 
 function isValidNumber(num) {
-  console.log('Ran isValidNumber');
+  
   return (Number(num) <= 50 && Number(num) >= 0 && /^\d+$/.test(Number(num)));
 }
 
+// function render(str){
+//   console.log(htmlStr.newStr);
+// //   $('.images').html(str);
+// }
+
 $(function() {
   console.log('App loaded! Waiting for submit!');
+  //render();
   watchForm();
 });
